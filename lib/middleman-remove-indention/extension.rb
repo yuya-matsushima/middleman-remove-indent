aa02cf87db426df1405b7eb240847958325614fa
yterajima <terra@e2esound.com>
2014-09-02 11:17:01 +0900

    Add remove indention feature

--- lib/middleman-remove-indention/extension.rb
+++ lib/middleman-remove-indention/extension.rb
@@ -1,12 +1,44 @@
 module Middleman
   module RemoveIndention
     class Extension < ::Middleman::Extension
-      def initialize(app, options = {}, &block)
+      option :exts, %w(.html .css), 'List of target file extensions'
+
+      def initialize(app, options_hash = {}, &block)
         super
 
-        app.after_build do |builder|
-          puts "Called After build"
+        build_dir = File.join(app.root, app.build_dir)
+        exts      = options.exts
+        extension = self
+
+        app.after_build do
+          targets = extension.target_files(build_dir, exts)
+          extension.update_file!(targets)
         end
+      end
+
+      def target_files(dir, exts)
+        paths = []
+        exts.each do |ext|
+          paths.concat(Dir.glob(dir + "/**/*#{ext}"))
+        end
+        paths
+      end
+
+      def update_file!(target_files)
+        target_files.each do |file|
+          open(file) do |f|
+            data = []
+            f.each do |line|
+              line = remove_indention(line)
+              data.push(line)
+            end
+            File.write(file, data.join("\n"))
+          end
+        end
+      end
+
+      def remove_indention(line)
+        line.gsub(/^(\s|\t)+/, '')
       end
     end
   end
--- middleman-remove-indention.gemspec
+++ middleman-remove-indention.gemspec
@@ -17,6 +17,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
+  spec.required_ruby_version = '>=1.9.3'
 
   spec.add_runtime_dependency "middleman", "~>3.3"
 
@@ -24,4 +25,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "aruba", "~> 0.6"
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "pry"
+  spec.add_development_dependency "pry-doc"
 end
